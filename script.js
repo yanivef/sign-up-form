fc0e2ae55c478bcaca7f21476a8aa8c1a0aacdb9
Yaniv Efroni <105995465+yanivef@users.noreply.github.com>
2023-09-20 14:25:13 +0300

    added js validation for password confirmation

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const inputs = document.querySelectorAll('.inp')
-const input = Array.from(inputs)
+const inputs = document.querySelectorAll(".inp");
+const input = Array.from(inputs);
+const btn = document.querySelector(".submit-btn");
+const pass = document.querySelector("#user-password");
+const confirmPass = document.querySelector("#user-confirm-password");
+const arr = [pass,confirmPass]
 
-input.forEach(inp => inp.addEventListener('input', () => {
-    if(inp.value)
-        inp.classList.add('validation')
+
+input.forEach((inp) =>
+  inp.addEventListener("input", () => {
+    if (inp.value && inp.type !== "password") inp.classList.add("validation");
+  })
+);
+
+
+arr.forEach(item => item.addEventListener('input', () => {
+    btn.disabled = true
+    
+    if(pass.value === confirmPass.value){
+        btn.disabled = false
+        item.classList.add('test') // NOW WE NEED TO ADD EFFECT WHEN PASSWORD IS CORRECT
+    }
+
 }))
+
